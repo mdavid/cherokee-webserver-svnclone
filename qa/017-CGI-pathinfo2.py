678255e229e2a5a68b5ba39de0b150faccbb52a4
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2005-10-24 12:26:28 +0000

    git-svn-id: svn://svn.cherokee-project.com/cherokee@68 5dc97367-97f1-0310-9951-d761b3857238

--- qa/017-CGI-pathinfo2.py
+++ qa/017-CGI-pathinfo2.py
@@ -21,4 +21,4 @@ class Test (TestBase):
                         echo "Content-Type: text/plain"
                         echo
                         echo "%s"
-                        """) % (MAGIC)
+                        """ % (MAGIC))
