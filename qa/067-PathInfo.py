abf748e4c2927206b7582755082ff451403fc58a
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2006-01-21 19:43:46 +0000

    git-svn-id: svn://svn.cherokee-project.com/cherokee@164 5dc97367-97f1-0310-9951-d761b3857238

--- qa/067-PathInfo.py
+++ qa/067-PathInfo.py
@@ -8,9 +8,21 @@ class Test (TestBase):
         self.name = "PathInfo, common"
 
         self.request           = "GET /pathinfo/test.php%s HTTP/1.0\r\n" %(PATH_INFO)
-        self.conf              = "Directory /pathinfo { Handler common }"
         self.expected_error    = 200
         self.expected_content  = "PathInfo is: "+PATH_INFO
+
+        self.conf              = """#
+                                    # If this test fails is probably due to an error in PHP5:
+                                    #
+                                    #        http://bugs.php.net/bug.php?id=31892
+                                    #
+                                    # - Work around: Append the following line to your php
+                                    # configuration file, usually /etc/php5/cgi/php.ini:
+                                    # 
+                                    #    cgi.fix_pathinfo=0
+                                    #
+                                    Directory /pathinfo { Handler common }
+                                 """
 
     def Prepare (self, www):
         self.Mkdir (www, "pathinfo")
