79907b31150a5065383ec39469f0abacc7ad109e
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-02-22 09:12:32 +0000

    Implements the MIME Types page
    git-svn-id: svn://svn.cherokee-project.com/cherokee/branches/adminctk@4288 5dc97367-97f1-0310-9951-d761b3857238

--- admin/PageMime.py
+++ admin/PageMime.py
@@ -39,16 +39,76 @@ NOTE_NEW_EXTENSIONS = N_('Comma separated list of file extensions associated wit
 NOTE_NEW_MAXAGE     = N_('Maximum time that this sort of content can be cached (in seconds).')
 
 def apply():
+    # New entry
+    mime = CTK.post.pop('new_mime')
+    exts = CTK.post.pop('new_exts')
+    mage = CTK.post.pop('new_mage')
+
+    if mime:
+        if mage:
+            CTK.cfg['mime!%s!max-age'%(mime)] = mage
+        if exts:
+            CTK.cfg['mime!%s!extensions'%(mime)] = exts
+
+        return {'ret': 'ok'}
+
     # Modifications
     for k in CTK.post:
         CTK.cfg[k] = CTK.post[k]
+
     return {'ret': 'ok'}
 
+
+class MIME_Table (CTK.Container):
+    def __init__ (self, refreshable, **kwargs):
+        CTK.Container.__init__ (self, **kwargs)
+
+        # List
+        table = CTK.Table ({'id': "mimetable"})
+        table.set_header(1)
+        table += [CTK.RawHTML(x) for x in (_('Mime type'), _('Extensions'), _('MaxAge<br/>(<i>secs</i>)'))]
+
+        mimes = CTK.cfg.keys('mime')
+        mimes.sort()
+
+        for mime in mimes:
+            pre = "mime!%s"%(mime)
+            e1 = CTK.TextCfgAuto ('%s!extensions'%(pre), URL_APPLY, False, {'size': 35})
+            e2 = CTK.TextCfgAuto ('%s!max-age'%(pre),    URL_APPLY, True,  {'size': 6, 'maxlength': 6})
+            rm = CTK.Image ({'src': '/CTK/images/del.png', 'alt': 'Del'})
+            table += [CTK.RawHTML(mime), e1, e2, rm]
+
+        self += CTK.Indenter (table)
+
+        # Add new MIME
+        table = CTK.PropsTable()
+        table.Add (_('Mime Type'),  CTK.TextField({'name': 'new_mime', 'class': 'noauto'}), _(NOTE_NEW_MIME))
+        table.Add (_('Extensions'), CTK.TextField({'name': 'new_exts', 'class': 'noauto optional'}), _(NOTE_NEW_EXTENSIONS))
+        table.Add (_('Max Age'),    CTK.TextField({'name': 'new_mage', 'class': 'noauto optional'}), _(NOTE_NEW_MAXAGE))
+
+        submit = CTK.Submitter(URL_APPLY)
+        submit += table
+        submit += CTK.SubmitterButton(_("Add"))
+        submit.bind ('submit_success', refreshable.JS_to_refresh())
+
+        self += CTK.RawHTML ('<h2>%s</h2>\n' %(_('Add new MIME')))
+        self += CTK.Indenter(submit)
+
+
+class MIME_Table_Instancer (CTK.Container):
+    def __init__ (self):
+        CTK.Container.__init__ (self)
+
+        # Refresher
+        refresh = CTK.Refreshable()
+        refresh.register (lambda: MIME_Table(refresh).Render())
+        self += refresh
 
 class Render():
     def __call__ (self):
         page = Page.Base(_('Icons'), helps=HELPS)
         page += CTK.RawHTML("<h1>%s</h1>" %(_('MIME Types')))
+        page += MIME_Table_Instancer()
         return page.Render()
 
 
