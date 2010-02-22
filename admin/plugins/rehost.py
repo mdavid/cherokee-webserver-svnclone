04e5346b0fc8d22d37d99b0a57a3e9ef6fbef9d9
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-02-22 15:00:23 +0000

    Implements the Rehost plug-in (Virtual Server matching rule based on regular expression patterns).
    git-svn-id: svn://svn.cherokee-project.com/cherokee/branches/adminctk@4297 5dc97367-97f1-0310-9951-d761b3857238

--- admin/plugins/rehost.py
+++ admin/plugins/rehost.py
@@ -22,9 +22,76 @@
 
 import CTK
 
-HELPS = []
+URL_APPLY = '/plugin/rehost/apply'
+
+NOTE_REHOST   = N_("Regular Expression against which the hosts be Host name will be compared.")
+WARNING_EMPTY = N_("At least one Regular Expression string must be defined.")
+
+def apply_del():
+    for k in CTK.post:
+        del (CTK.cfg[k])
+    return {'ret':'ok'}
+
+
+class Content (CTK.Container):
+    def __init__ (self, refreshable, key, url_apply, **kwargs):
+        CTK.Container.__init__ (self, **kwargs)
+
+        # List
+        entries = CTK.cfg.keys (key)
+        if entries:
+            table  = CTK.Table()
+            submit = CTK.Submitter(url_apply)
+
+            submit += table
+            self += CTK.Indenter(submit)
+
+            table.set_header(1)
+            table += [CTK.RawHTML(_('Regular Expressions'))]
+
+            for i in entries:
+                e1 = CTK.TextCfg ("%s!%s"%(key,i))
+                rm = None
+                if len(entries) >= 2:
+                    rm = CTK.Image ({'src':'/CTK/images/del.png', 'alt':'Del'})
+                    rm.bind('click', CTK.JS.Ajax (url_apply,
+                                                  data     = {"%s!%s"%(key,i): ''},
+                                                  complete = refreshable.JS_to_refresh()))
+                table += [e1, rm]
+
+        # Add new
+        table = CTK.PropsTable()
+        next  = CTK.cfg.get_next_entry_prefix (key)
+        table.Add (_('New Regular Expression'), CTK.TextCfg(next, False, {'class':'noauto'}), _(NOTE_REHOST))
+
+        submit = CTK.Submitter(url_apply)
+        submit += table
+        submit += CTK.SubmitterButton(_('Add'))
+        submit.bind ('submit_success', refreshable.JS_to_refresh())
+
+        self += CTK.RawHTML("<h3>%s</h3>" %(_('Add new')))
+        self += CTK.Indenter(submit)
+
 
 class Plugin_rehost (CTK.Plugin):
     def __init__ (self, key, vsrv_num):
         CTK.Plugin.__init__ (self, key)
 
+        pre       = '%s!regex' %(key)
+        url_apply = '%s/%s' %(URL_APPLY, vsrv_num)
+
+        self += CTK.RawHTML ("<h2>%s</h2>" % (_('New Regular Expression')))
+
+        # Content
+        entries = CTK.cfg.keys(pre)
+        if not entries:
+            notice = CTK.Notice('warning')
+            notice += CTK.RawHTML (WARNING_EMPTY)
+            self += notice
+
+        refresh = CTK.Refreshable()
+        refresh.register (lambda: Content(refresh, pre, url_apply).Render())
+        self += refresh
+
+        # Validation, and Public URLs
+        CTK.publish ('^%s/[\d]+$'%(URL_APPLY), self.apply, method="POST")
