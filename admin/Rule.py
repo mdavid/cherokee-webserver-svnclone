80bc5440681a48efc41c766447eff0949789655a
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-03-08 13:12:51 +0000

    WIP: Rules

--- admin/Rule.py
+++ admin/Rule.py
@@ -118,6 +118,7 @@ class Rule (CTK.Box):
         elif value == "and":
             self.props['class'] += ' rule-and'
             self += Rule ('%s!left' %(self.key), self.depth+1)
+            self += CTK.Box({'id':'rule-label-and'}, CTK.RawHTML(_("And")))
             self += Rule ('%s!right'%(self.key), self.depth+1)
             self += RuleButtons (self.key, self.depth)
             return CTK.Box.Render (self)
@@ -125,12 +126,14 @@ class Rule (CTK.Box):
         elif value == "or":
             self.props['class'] += ' rule-or'
             self += Rule ('%s!left' %(self.key), self.depth+1)
+            self += CTK.Box({'id':'rule-label-or'}, CTK.RawHTML(_("Or")))
             self += Rule ('%s!right'%(self.key), self.depth+1)
             self += RuleButtons (self.key, self.depth)
             return CTK.Box.Render (self)
 
         elif value == "not":
             self.props['class'] += ' rule-not'
+            self += CTK.Box({'id':'rule-label-not'}, CTK.RawHTML(_("Not")))
             self += Rule ('%s!right'%(self.key), self.depth+1)
             self += RuleButtons (self.key, self.depth)
             return CTK.Box.Render (self)
