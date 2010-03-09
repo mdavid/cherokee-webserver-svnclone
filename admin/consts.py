e69ba1491072cc431235689c8411e494c5421b99
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-03-09 08:01:09 +0000

    New plugin, + fixes

--- admin/consts.py
+++ admin/consts.py
@@ -140,19 +140,39 @@ REDIR_SHOW = [
 
 ERROR_CODES = [
     ('400', '400 Bad Request'),
+    ('401', '401 Unauthorized'),
+    ('402', '402 Payment Required'),
     ('403', '403 Forbidden'),
     ('404', '404 Not Found'),
     ('405', '405 Method Not Allowed'),
+    ('406', '406 Not Acceptable'),
+    ('407', '407 Proxy Auth Required'),
+    ('408', '408 Request Timeout'),
+    ('409', '409 Conflict'),
     ('410', '410 Gone'),
+    ('411', '411 Length Required'),
+    ('412', '412 Precondition Failed'),
     ('413', '413 Request Entity too large'),
     ('414', '414 Request-URI too long'),
+    ('415', '415 Unsupported Media Type'),
     ('416', '416 Requested range not satisfiable'),
+    ('417', '417 Expectation Failed'),
+    ('422', '422 Unprocessable Entity'),
+    ('423', '423 Locked'),
+    ('424', '424 Failed Dependency'),
+    ('425', '425 Unordered Collection'),
+    ('426', '426 Upgrade Required'),
+    ('449', '449 Retry With'),
     ('500', '500 Internal Server Error'),
     ('501', '501 Not Implemented'),
     ('502', '502 Bad gateway'),
     ('503', '503 Service Unavailable'),
     ('504', '504 Gateway Timeout'),
-    ('505', '505 HTTP Version Not Supported')
+    ('505', '505 HTTP Version Not Supported'),
+    ('506', '506 Variant Also Negotiates'),
+    ('507', '507 Insufficient Storage'),
+    ('509', '509 Bandwidth Limit Exceeded'),
+    ('510', '510 Not Extended')
 ]
 
 RULES = [
@@ -165,7 +185,7 @@ RULES = [
     ('bind',       N_('Incoming IP/Port')),
     ('fullpath',   N_('Full Path')),
     ('from',       N_('Connected from')),
-#    ('url_arg',    N_('URL Argument')),
+    ('url_arg',    N_('URL Argument')),
 #    ('geoip',      N_('GeoIP'))
 ]
 
