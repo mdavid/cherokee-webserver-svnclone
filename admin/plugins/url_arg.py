51e0f1c37e8c687c5ddfb4f492c54f50b040ed8a
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-03-09 08:01:09 +0000

    New plugin, + fixes
    git-svn-id: svn://svn.cherokee-project.com/cherokee/branches/adminctk@4424 5dc97367-97f1-0310-9951-d761b3857238

---
+++ admin/plugins/url_arg.py
@@ -0,0 +1,99 @@
+# Cheroke Admin
+#
+# Authors:
+#      Alvaro Lopez Ortega <alvaro@alobbs.com>
+#
+# Copyright (C) 2010 Alvaro Lopez Ortega
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public
+# License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+import CTK
+
+from Rule import RulePlugin
+from util import *
+
+URL_APPLY = '/plugin/url_arg/apply'
+
+NOTE_ARGUMENT = N_("Argument name")
+NOTE_REGEX    = N_("Regular Expression for the match")
+
+OPTIONS = [('0', _('Match a specific argument')),
+           ('1', _('Match any argument'))]
+
+def apply():
+    # POST info
+    key       = CTK.post.pop ('key', None)
+    vsrv_num  = CTK.post.pop ('vsrv_num', None)
+    new_match = CTK.post.pop ('tmp!match', None)
+    new_any   = CTK.post.pop ('tmp!match_any', "0")
+    new_arg   = CTK.post.pop ('tmp!arg', None)
+
+    # New entry
+    if new_match:
+        next_rule, next_pre = cfg_vsrv_rule_get_next ('vserver!%s'%(vsrv_num))
+
+        CTK.cfg['%s!match'%(next_pre)]           = 'url_arg'
+        CTK.cfg['%s!match!match'%(next_pre)]     = new_match
+        CTK.cfg['%s!match!match_any'%(next_pre)] = new_any
+
+        if not int(new_any):
+            CTK.cfg['%s!match!arg'%(next_pre)]   = new_arg
+
+        return {'ret': 'ok', 'redirect': '/vserver/%s/rule/%s' %(vsrv_num, next_rule)}
+
+    # Modifications
+    for k in CTK.post:
+        CTK.cfg[k] = CTK.post[k]
+    return {'ret': 'ok'}
+
+
+class Plugin_url_arg (RulePlugin):
+    def __init__ (self, key, **kwargs):
+        RulePlugin.__init__ (self, key)
+        if key.startswith('tmp'):
+            props = {'class': 'noauto'}
+        else:
+            props = {}
+
+        any   = CTK.ComboCfg('%s!match_any'%(key), OPTIONS, props)
+        table = CTK.PropsTable()
+        table.Add (_('Match type'), any, '')
+        table.Add (_('Argument'),           CTK.TextCfg('%s!arg'%(key), False, props), _(NOTE_ARGUMENT))
+        table.Add (_('Regular Expression'), CTK.TextCfg('%s!match'%(key), False, props), _(NOTE_REGEX))
+
+        # Special events
+        any.bind ('change', """if ($(this).val() == 0) {
+                                 $("#%(row)s").show();
+                               } else {
+                                 $("#%(row)s").hide(); }""" %({'row': table[1].id}))
+
+        submit = CTK.Submitter (URL_APPLY)
+        submit += CTK.Hidden ('key', key)
+        submit += CTK.Hidden ('vsrv_num', kwargs.pop('vsrv_num', ''))
+        submit += table
+        self += submit
+
+        # Validation, and Public URLs
+        CTK.publish (URL_APPLY, apply, method="POST")
+
+    def GetName (self):
+        match = CTK.cfg.get_val ('%s!match'%(self.key), '')
+
+        if int(CTK.cfg.get_val ('%s!match_any'%(self.key), "0")):
+            return "Any arg. matches ~ %s" %(match)
+
+        arg = CTK.cfg.get_val ('%s!arg'%(self.key), '')
+        return "Arg %s matches %s" %(arg, match)
