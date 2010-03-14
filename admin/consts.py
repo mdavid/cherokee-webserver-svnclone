31f47aba0064870fc41e822d31a83657992eebce
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-03-14 11:34:59 +0000

    New Custom Error plug-in

--- admin/consts.py
+++ admin/consts.py
@@ -59,7 +59,7 @@ HANDLERS = [
     ('secdownload',  N_('Hidden Downloads')),
     ('server_info',  N_('Server Info')),
 #    ('dbslayer',     N_('MySQL bridge')),
-#    ('custom_error', N_('HTTP error')),
+    ('custom_error', N_('HTTP error')),
 #    ('admin',        N_('Remote Administration')),
 #    ('empty_gif',    N_('1x1 Transparent GIF'))
 ]
