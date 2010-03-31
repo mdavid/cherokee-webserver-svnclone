f744364fb4f05125e8936c5863911add4010371b
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-03-31 09:46:34 +0000

    Fixes a parsing issue, + a few (picky) style changes.

--- admin/PageHelp.py
+++ admin/PageHelp.py
@@ -4,6 +4,7 @@
 #
 # Authors:
 #      Taher Shihadeh <taher@unixwars.com>
+#      Alvaro Lopez Ortega <alvaro@alobbs.com>
 #
 # Copyright (C) 2010 Alvaro Lopez Ortega
 #
@@ -30,34 +31,39 @@ import configured
 
 from CTK.consts import *
 
-# Quitar html de la regla de cherokee
 URL_BASE   = '/help'
 TOC_TITLE  = N_('Table of Contents')
 
-BODY_REGEX = """<div class="sectionbody">(.+?)</div>\s*<div id="footer">"""
-LINK_REGEX = """<a href="(.+?)">(.+?)</a>""" #link, title
+BODY_REGEX = r'<div class="sectionbody">(.+?)</div>\s*<div id="footer">'
+LINK_REGEX = r'<a href="(.+?)">(.+?)</a>' #link, title
+
+HEADERS    = ['<link href="media/css/cherokee_doc.css" rel="stylesheet" type="text/css" />']
+
 
 class Parser():
     """Simple documentation parser"""
     def __init__ (self, filename):
         self.filename = filename
-        filepath  = os.path.join(configured.DOCDIR, filename)
-        self.html = open(filepath).read()
-        self.body = self.parse (BODY_REGEX, self.html)[0]
         self.sections = None
 
-    def parse (self, regex, txt):
+        filepath  = os.path.join (configured.DOCDIR, filename)
+        self.html = open(filepath).read()
+
+        body = self._parse (BODY_REGEX, self.html)[0]
+        self.body = body.replace ('%', '%%')
+
+    def _parse (self, regex, txt):
         return re.findall (regex, txt, re.DOTALL)
 
     def get_sections (self):
         if self.sections:
             return self.sections
 
-        self.build_tree()
-        self.sections = self.build_section (self.filename, _(TOC_TITLE))
+        self._build_tree()
+        self.sections = self._build_section (self.filename, _(TOC_TITLE))
         return self.sections
 
-    def build_section (self, link, text):
+    def _build_section (self, link, text):
         """
         entry = { 'link': link, 'text': title, 'children': [entry ...]}
         """
@@ -67,11 +73,11 @@ class Parser():
         if childlinks:
             for lnk,txt in self.links:
                 if lnk in childlinks:
-                    children.append (self.build_section (lnk, txt))
+                    children.append (self._build_section (lnk, txt))
         entry['children'] = children
         return entry
 
-    def build_tree (self):
+    def _build_tree (self):
         def branch_sorter (x, y):
             return len(y) - len(x)
 
@@ -87,50 +93,53 @@ class Parser():
                 tmp.extend(vals)
             return len(tmp)
 
-        self.links = self.parse (LINK_REGEX, self.body)
+        self.links = self._parse (LINK_REGEX, self.body)
         self.files = [x[0] for x in self.links]
-        self.tree = {'index.html': [f for f in self.files if len(f.split('_'))==1]}
-        delta     = 1
+        self.tree  = {'index.html': [f for f in self.files if len(f.split('_'))==1]}
+        delta      = 1
 
         while tree_len() < len(self.links):
             mess  = [(f, f.split('.html')[0].split('_')) for f in
                      self.files if not is_covered(f)]
             mess.sort (branch_sorter)
-            self.build_helper (mess, delta)
+            self._build_helper (mess, delta)
             delta += 1
 
-    def build_helper (self, mess, delta):
+    def _build_helper (self, mess, delta):
         for elem in mess:
             x = elem[1]
             name, idx = None, len(x)-delta
             if idx >= 0:
-                name = '%s.html'%('_'.join(x[:idx]))
+                name = '%s.html' %('_'.join(x[:idx]))
             if name in self.files:
                 self.tree[name] = self.tree.get(name, [])
-                childname = '%s.html'%('_'.join(x))
+                childname = '%s.html' %('_'.join(x))
                 self.tree[name].append(childname)
 
 
 class IndexBox (CTK.Box):
     def __init__ (self, link, **kwargs):
         CTK.Box.__init__ (self, {'class': 'help_toc'}, **kwargs)
-        parser        = Parser ('index.html')
-        self.link     = link
-        self.sections = parser.get_sections ()
-        self         += self.process_entry (self.sections)
 
-    def process_entry (self, entry):
+        self.link     = link
+        parser        = Parser ('index.html')
+        self.sections = parser.get_sections()
+
+        # Widget content (the index)
+        self += self._process_entry (self.sections)
+
+    def _process_entry (self, entry):
         props = {}
         if entry['link'] == self.link:
             props['class'] = 'help_selected'
         block  = CTK.Table (props)
-        block += [CTK.RawHTML(LINK_HREF%(entry['link'],entry['text']))]
+        block += [CTK.RawHTML (LINK_HREF %(entry['link'],entry['text']))]
 
         spawn = entry.get('children')
         if spawn:
             children = CTK.Container()
             for child in spawn:
-                children += self.process_entry (child)
+                children += self._process_entry (child)
             block += [CTK.Indenter (children)]
 
         return block
@@ -142,20 +151,6 @@ class HelpBox (CTK.Box):
         parser  = Parser (filename)
         self += CTK.RawHTML (parser.body)
 
-
-class Render():
-    def __call__ (self):
-        # Content
-        filename = CTK.request.url.split('/')[-1]
-        left  = IndexBox(filename)
-        right = HelpBox (filename)
-
-        # Build the page
-        page = Page ()
-        page += left
-        page += right
-
-        return page.Render()
 
 class Page (CTK.Page):
     def __init__ (self, **kwargs):
@@ -173,4 +168,19 @@ class Page (CTK.Page):
         CTK.Page.__init__ (self, template, **kwargs)
 
 
-CTK.publish ('^%s/.+\.html'   %(URL_BASE), Render)
+class Render():
+    def __call__ (self):
+        # Content
+        filename = CTK.request.url.split('/')[-1]
+        left  = IndexBox(filename)
+        right = HelpBox (filename)
+
+        # Build the page
+        page = Page (headers = HEADERS)
+        page += left
+        page += right
+
+        return page.Render()
+
+
+CTK.publish ('^%s/.+\.html'%(URL_BASE), Render)
