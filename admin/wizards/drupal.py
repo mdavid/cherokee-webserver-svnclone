4b46f105783428dbaca2c9d32d511acc0f965357
alo <alo@5dc97367-97f1-0310-9951-d761b3857238>
2010-04-05 13:07:24 +0000

    Implements the PHP and Drupal wizards, + some improvements.

--- admin/wizards/drupal.py
+++ admin/wizards/drupal.py
@@ -51,17 +51,80 @@ SRC_PATHS = [
     "/usr/local/www/data/drupal*" # BSD
 ]
 
+CONFIG_VSERVER = """
+%(pre_vsrv)s!nick = %(host)s
+%(pre_vsrv)s!document_root = %(local_dir)s
+%(pre_vsrv)s!directory_index = index.php,index.html
+
+%(pre_rule_plus3)s!match = request
+%(pre_rule_plus3)s!match!request = ^/([0-9]+)$
+%(pre_rule_plus3)s!handler = redir
+%(pre_rule_plus3)s!handler!rewrite!1!regex = ^/([0-9]+)$
+%(pre_rule_plus3)s!handler!rewrite!1!show = 0
+%(pre_rule_plus3)s!handler!rewrite!1!substring = /index.php?q=/node/$1
+
+%(pre_rule_plus2)s!match = request
+%(pre_rule_plus2)s!match!request = \.(engine|inc|info|install|module|profile|test|po|sh|.*sql|theme|tpl(\.php)?|xtmpl|svn-base)$|^(code-style\.pl|Entries.*|Repository|Root|Tag|Template|all-wcprops|entries|format)$
+%(pre_rule_plus2)s!handler = custom_error
+%(pre_rule_plus2)s!handler!error = 403
+
+%(pre_rule_plus1)s!match = fullpath
+%(pre_rule_plus1)s!match!fullpath!1 = /
+%(pre_rule_plus1)s!handler = redir
+%(pre_rule_plus1)s!handler!rewrite!1!show = 0
+%(pre_rule_plus1)s!handler!rewrite!1!substring = /index.php
+
+# IMPORTANT: The PHP rule comes here
+
+%(pre_rule_minus1)s!match = exists
+%(pre_rule_minus1)s!match!iocache = 1
+%(pre_rule_minus1)s!match!match_any = 1
+%(pre_rule_minus1)s!handler = file
+
+%(pre_rule_minus2)s!match = default
+%(pre_rule_minus2)s!handler = redir
+%(pre_rule_minus2)s!handler!rewrite!1!show = 0
+%(pre_rule_minus2)s!handler!rewrite!1!regex = ^/(.*)\?(.*)$
+%(pre_rule_minus2)s!handler!rewrite!1!substring = /index.php?q=$1&$2
+%(pre_rule_minus2)s!handler!rewrite!2!show = 0
+%(pre_rule_minus2)s!handler!rewrite!2!regex = ^/(.*)$
+%(pre_rule_minus2)s!handler!rewrite!2!substring = /index.php?q=$1
+"""
+
 
 def commit():
     if CTK.post.pop('final'):
-        ret = CTK.cfg_apply_post()
+        # Apply POST
+        CTK.cfg_apply_post()
 
+        # Create the new Virtual Server
         next = CTK.cfg.get_next_entry_prefix('vserver')
         CTK.cfg['%s!nick'%(next)] = CTK.cfg.get_val('%s!host'%(PREFIX))
+        Wizard.CloneLogsCfg_Apply ('%s!logs_as_vsrv'%(PREFIX), next)
 
-        del(CTK.cfg[PREFIX])
-        print "ret", ret
-        return ret
+        # PHP
+        php = CTK.load_module ('php', 'wizards')
+        error = php.wizard_php_add (next)
+        php_info = php.get_info (next)
+
+        # Drupal
+        props = cfg_get_surrounding_repls ('pre_rule', php_info['rule'])
+        props['pre_vsrv']  = next
+        props['host']      = CTK.cfg.get_val('%s!host'      %(PREFIX))
+        props['local_dir'] = CTK.cfg.get_val('%s!local_dir' %(PREFIX))
+
+        config = CONFIG_VSERVER %(props)
+        CTK.cfg.apply_chunk (config)
+
+        # Fixes Drupal bug for multilingual content
+        CTK.cfg['%s!encoder!gzip' %(php_info['rule'])] = '0'
+
+        # Clean up
+        CTK.cfg.normalize ('%s!rule'%(next))
+        CTK.cfg.normalize ('vserver')
+
+        del (CTK.cfg[PREFIX])
+        return {'ret': 'ok'}
 
     return CTK.cfg_apply_post()
 
@@ -69,7 +132,7 @@ def commit():
 class Host:
     def __call__ (self):
         table = CTK.PropsTable()
-        table.Add (_('New Host Name'),    CTK.TextCfg ('%s!host'%(PREFIX), False, {'value': 'www.example.com'}), NOTE_HOST)
+        table.Add (_('New Host Name'),    CTK.TextCfg ('%s!host'%(PREFIX), False, {'value': 'www.example.com', 'class': 'noauto'}), NOTE_HOST)
         table.Add (_('Use Same Logs as'), Wizard.CloneLogsCfg('%s!logs_as_vsrv'%(PREFIX)), _(Wizard.CloneLogsCfg.NOTE))
 
         submit = CTK.Submitter (URL_APPLY)
